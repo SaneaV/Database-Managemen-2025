76500850209fc89529bfbbe73085f82727d39d53
Dima Zalucean <dimazalucean@Dumitrus-MacBook-Pro.local>
2025-05-24 11:48:42 +0300

    Create project structure

---
+++ retail-be/src/main/java/com/utm/retail_be/RetailBeApplication.java
@@ -0,0 +1,13 @@
+package com.utm.retail_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailBeApplication.class, args);
+	}
+
+}
